77f6f356f90cc59b094b0a4e7e602b5187a4759a
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2021-11-07 21:19:48 +0100

    mongoengine database configuration

--- myxapp/myxapp/settings.py
+++ myxapp/myxapp/settings.py
@@ -1,6 +1,10 @@
 
 
 from pathlib import Path
+import mongoengine
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=_0+rjm4qys=5a633l19(tl0oej73je&9=7soubw59kex2st58'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -66,12 +70,16 @@ WSGI_APPLICATION = 'myxapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {   #Comment this out if you intend to use mongoengine
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+mongoengine.connect(
+    host=os.getenv('HOST')
+)
 
 
 # Password validation
