7c2e0dadf381885d21b0e82e26108f2b1550115b
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2021-11-08 21:41:52 +0100

    added views and configured urls

--- myxapp/api/serializer.py
+++ myxapp/api/serializer.py
@@ -1,4 +1,10 @@
-from rest_framework_mongoengine.serializers import DocumentSerializer
+from rest_framework import serializers
+from rest_framework_mongoengine import serializers
 from . models import User
 
+class UserSerializer(serializers.DocumentSerializer):
+    """ Serializing our User Object Document-> Import this to the view.py of our app for usage """
+    class Meta:
+        model = User
+        fields = ('fname', 'lname','email','password')
 
