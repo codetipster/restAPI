94296b93a24be8e5e0c1915bfebb2b9fe92803d9
Samuel Nzekwe <talk2samuelnzekwe@gmail.com>
2021-11-08 14:08:43 +0100

    models.py and serializers

--- myxapp/api/models.py
+++ myxapp/api/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+# from django.db import models
+from mongoengine import *
+import datetime
 
+from mongoengine.document import Document, EmbeddedDocument, EmbeddedDocumentField
+from mongoengine.fields import EmailField, StringField, DateTimeField, BooleanField, ListField, ReferenceField
 # Create your models here.
+
+class User(Document):
+    """ A User document that defines fields for our app users """
+
+    fname = StringField(max_length=50, required=True)
+    lname = StringField(max_length=50, required=True)
+    email = EmailField(max_length=255, unique=True)
+    password = StringField(max_length=100, required=True)
+    created_at = DateTimeField(default=datetime.datetime.utcnow())
+    is_active = BooleanField(default=True)
+
+class UserInterests(EmbeddedDocument):
+    pass
+
+class UserExpertAreas(EmbeddedDocument):
+    pass
+
+class UserInfo(EmbeddedDocument):
+    pass
+
+
+class Profile(Document): #Upon authentication, users should be able to create profile
+    """Every User sets their profile with this model"""
+
+    user = ReferenceField(User) #Every user has a profile
+    interested_in = ListField(EmbeddedDocumentField(UserInterests))
+    expert_in = ListField(EmbeddedDocumentField(UserExpertAreas))
+    basic_info = ListField(EmbeddedDocumentField(UserInfo))
